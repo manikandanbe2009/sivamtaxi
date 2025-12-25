72450878d5d84281760515fbbc0c252420db99ca
Manikandan <manikandanbe2009@gmail.com>
2025-12-25 22:43:52 +0530

    all data

--- src/components/BootstrapClient.jsx
+++ src/components/BootstrapClient.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useEffect } from "react";
@@ -10,16 +9,3 @@ export default function BootstrapClient() {
 
   return null;
 }
-=======
-"use client";
-
-import { useEffect } from "react";
-
-export default function BootstrapClient() {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
-  return null;
-}
->>>>>>> 67886b3d4e54dfae9e1864a230271ec22ed9722b
